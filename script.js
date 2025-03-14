ec489b524ae8126ac36c824edf0686819551443b
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-03-14 11:52:46 -0400

    Change all innerHTML to textContent add script to trigger read from yes to no

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ function addBookToLibrary(a) {
     main.appendChild(card);
 
     const title = document.createElement("div");
-    title.innerHTML = "Title: " + JSON.stringify(myLibrary[myLibrary.length-1].title);
+    title.textContent = "Title: " + JSON.stringify(myLibrary[myLibrary.length-1].title);
     const author = document.createElement("div");
-    author.innerHTML = "author: " + JSON.stringify(myLibrary[myLibrary.length-1].author);
+    author.textContent = "author: " + JSON.stringify(myLibrary[myLibrary.length-1].author);
     const pages = document.createElement("div");
-    pages.innerHTML = "Number of pages: " + JSON.stringify(myLibrary[myLibrary.length-1].pages);
+    pages.textContent = "Number of pages: " + JSON.stringify(myLibrary[myLibrary.length-1].pages);
     const read = document.createElement("div");
-    read.innerHTML = "Finished: " + JSON.stringify(myLibrary[myLibrary.length-1].read);
+    read.textContent = "Finished: " + JSON.stringify(myLibrary[myLibrary.length-1].read);
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
@@ -31,17 +31,34 @@ function addBookToLibrary(a) {
 
     const id = myLibrary[myLibrary.length-1].id;
 
-    const remove = document.createElement("button");
-    remove.addEventListener("click", () => {
-        main.removeChild(card);
+    const finished = document.createElement("button");
+    finished.textContent = "Finished";
+    finished.addEventListener("click", () => {
         for (i=0; i<myLibrary.length; i++) {
             if (myLibrary[i].id == id) {
-                myLibrary.splice(i, 1);
-            }
+                if (myLibrary[i].read == "Yes") {
+                    myLibrary[i].read = "No";
+                    read.textContent = "Finished: " + JSON.stringify(myLibrary[i].read);
+                } else {
+                    myLibrary[i].read = "Yes"
+                    read.textContent = "Finished: " + JSON.stringify(myLibrary[i].read);
+                }
+            };
         }
-    }
-    );
-    card.appendChild(remove)
+    });
+    card.appendChild(finished);
+
+    const remove = document.createElement("button");
+    remove.textContent = "Delete";
+    remove.addEventListener("click", () => {
+            main.removeChild(card);
+                for (i=0; i<myLibrary.length; i++) {
+                    if (myLibrary[i].id == id) {
+                        myLibrary.splice(i, 1);
+                };
+            };
+    });
+    card.appendChild(remove);
 }
 
 main = document.querySelector(".main");
